61457e99a3c06948a1cf05f262b9ad2b8b3ed273
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-02-10 10:41:35 -0800

    'initial-commit'

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.querySelector(".card-1").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Flash-Cards");
+});
+document.querySelector(".card-2").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Simple-Calculator");
+});
+document.querySelector(".card-3").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Stiky-notes");
+});
+document.querySelector(".card-4").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Timer");
+});
+document.querySelector(".card-5").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Math");
+});
+document.querySelector(".card-6").addEventListener("click", function () {
+  window.open("https://github.com/eeyfesubalew/Simple-Calculator");
+});
